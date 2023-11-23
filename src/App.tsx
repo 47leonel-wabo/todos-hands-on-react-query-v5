5559175e14cd746dc3820630b0ed3c685a1e918c
leonel47 <waboleonel@gmail.com>
2023-11-23 09:00:22 +0100

    Adding chakra-I and defining the grid system

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Grid
+      templateAreas={{
+        base: `
+        "navbar"
+        "main"
+        "footer"
+        `,
+        md: `
+        "navbar navbar"
+        "aside main"
+        "aside footer"
+        `,
+        xl: `
+        "navbar navbar navbar"
+        "aside main nav"
+        "aside footer nav"
+        `,
+      }}
+      gridTemplateColumns={{
+        base: "1fr",
+        md: "200px 1fr",
+        xl: "400px 1fr 100px",
+      }}
+      gap={1}
+    >
+      <GridItem area={"navbar"} bg="orange.300">
+        Navbar
+      </GridItem>
+      <GridItem area={"main"} bg="blue.300">
+        Main
+      </GridItem>
+      <Show above="md">
+        <GridItem area={"aside"} bg="gray.300">
+          Aside
+        </GridItem>
+      </Show>
+      <Show above="xl">
+        <GridItem area={"nav"} bg="green.300">
+          Left nav
+        </GridItem>
+      </Show>
+      <GridItem area={"footer"} bg="red.300">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
