373611bd5106ee47701c7c58a61eeadbe9d98a94
leonel47 <waboleonel@gmail.com>
2023-11-23 10:11:11 +0100

    Building navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,27 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Box, HStack, Heading, Switch, useColorMode } from "@chakra-ui/react";
+import { MdNightsStay, MdOutlineLightMode } from "react-icons/md";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === "light" ? "Dark" : "Light"}
-      </Button>
-    </div>
+    <HStack justifyContent={"space-between"}>
+      <Box>
+        <Heading size="lg" color={"tomato"} marginX={2}>
+          Todos
+        </Heading>
+      </Box>
+      <Box display={"flex"} alignItems={"center"} padding={2}>
+        <Switch
+          colorScheme="green"
+          isChecked={colorMode === "dark"}
+          size={"sm"}
+          onChange={toggleColorMode}
+        />
+        <Box marginX={2}>
+          {colorMode === "light" ? <MdOutlineLightMode /> : <MdNightsStay />}
+        </Box>
+      </Box>
+    </HStack>
   );
 };
 
